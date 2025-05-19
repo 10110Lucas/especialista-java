80c46488b0594371ae716676c47d76d9b2d05d32
10110Lucas <lucas_900azul@hotmail.com>
2025-05-19 00:57:26 -0300

    feat: Uso de escapes

--- 02.20. Usando sequências de escape/SequenciasEscape.java
+++ 02.20. Usando sequências de escape/SequenciasEscape.java
@@ -3,7 +3,8 @@ public class SequenciasEscape {
   public static void main(String[] args) {
     System.out.println("Oi \"Maria\"");
 
-    System.out.println("Seu nome:\nJoão");
+    // pula linha e da uma tabulação
+    System.out.println("Seu nome:\n\t-João");
 
     System.out.println("C:\\Windows");
   }
